4ac465d00f8d2539ac2f2fb66f7ba26cf5a886f0
Ankit Vashishta <ankitjobalerts@gmail.com>
2024-04-09 23:02:27 +0530

    Added Protype Inheritance in Main.js

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ function callMe(greeting, wellbeing) {
 }
 
 callMe(greet("Vinod"), "HOw are you");
+
+/*
+Prototype are of two types
+
+1. Built In Protypes
+2. Custom Protypes
+*/
+
+const myHunting = {
+    name: "ANkit",
+    greet() {
+        console.log("HI " + this.name);
+    }
+}
+
+const myWellBeing = {
+    name: "Vinod",
+    wellBing() {
+        console.log("How are you");
+    }
+}
+
+myHunting.__proto__ = myWellBeing;
+
+myHunting.wellBing();
