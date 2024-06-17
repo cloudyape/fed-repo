3fa7ba88acefb8b0b7394f98a8109722bc99e2fc
Ankit Vashishta <ankitjobalerts@gmail.com>
2024-06-17 09:15:28 +0530

    fed code is pushed by ankit

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
-let a = [23, 45, 67, 89, 12, 34, 56, 78, 90, 21, 43, 65, 87, 98, 10, 32, 54, 76, 98, 19, 41, 63, 85, 96, 18, 40, 62, 84, 95, 17, 39, 61, 83, 94, 16, 38, 60, 82, 93, 15, 37, 59, 81, 92, 14, 36, 58, 80, 91, 13, 35, 57, 79, 100, 22, 44, 66, 88, 11, 33, 55, 77, 99, 20, 42, 64, 86, 97, 29, 51, 73, 95, 100, 30, 52, 74, 96, 101, 31, 53, 75, 97, 102, 32, 54, 76, 98, 103, 33, 55, 77, 99, 104, 34, 56, 78, 100, 105, 35, 57, 79, 101, 106, 36, 58, 80, 102, 107, 37, 59, 81, 103, 108, 38, 60, 82, 104, 109, 39];
+/*
+Javascript Datatypes
 
-console.log(a[a.length - 1]);
+1. string
+2. number
+3. bigint
+4. boolean
+5. object
+6. array
+7. date
+
+*/
+
+var a = "Ankit Vashsihta";
+var b = 'vb 322 #@@$@#';
+
+var c = 4; //Interger format
+var d = 3.14; // float format
+var e = 4 + i; //Complex Numbers
+
+var f = 953748245039840238423890524850428592050;
+
+var g = true;
+var h = false;
+
+/*JSON - Javascript Object Notation
+
+Object are the set of Key Value Pairs in Javascript
+*/
+
+var i = {
+    name: "ANkit",
+    age : 43,
+    isMale: true
+}
+
+/*
+Array is Collection of Multiple Data Types
+
+*/
+
+var j = ["Ankit", 4, true, {namae: "John", age: 34}];
+
+/*
+date - Date represent current datetime
+*/
+
+var k = new Date("24 July 1997 06:45:34");
+
+console.log(k);
